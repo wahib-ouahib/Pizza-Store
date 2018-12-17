0a53539a852dcf96392f59555ec008adbd5e649b
wahib <wahib.med.ouahib@gmail.com>
2018-12-17 21:24:45 +0000

    rendering the data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <h1>Welcome to the pizza Store</h1>
+          <div className="img">
+            <img src={require("./assets/1.jpg")} alt="description of " />
+            <h2>4 saison <br/> <br/> 15$ <br/> 500calories</h2>
+          </div>
+
+          <div className="img">
+            <img src={require("./assets/2.PNG")} alt="description of " />
+            <h2>4 saison <br/> <br/> 15$ <br/> 500calories</h2>
+          </div>
+
+          <div className="img">
+            <img src={require("./assets/3.PNG")} alt="description of " />
+            <h2>4 saison <br/> <br/> 15$ <br/> 500calories</h2>
+          </div>
+
+          <div className="img">
+            <img src={require("./assets/4.PNG")} alt="description of " />
+            <h2>4 saison <br/> <br/> 15$ <br/> 500calories</h2>
+          </div>
+
+          <div className="img">
+            <img src={require("./assets/5.PNG")} alt="description of " />
+            <h2>4 saison <br/> <br/> 15$ <br/> 500calories</h2>
+          </div>          
       </div>
     );
   }
